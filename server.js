7a42c7ee1fd6439da2f142422739606bc3289000
merry75 <benjiab@hotmail.com>
2017-03-26 10:26:26 +0300

    authentification with passport.js

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var LocalStrategy = require('passport-local').Strategy;
+var expressSession = require('express-session');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressSession({ secret: 'thisIsASecret', resave: false, saveUninitialized: false }));;
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if ((username === "John") && (password === "password")) {
+      return done(null, { username: username, id: 1 });
+    } else {
+      return done(null, false);
+    }
+  }
+));
+
+app.get('/success', function (req, res){
+  if (req.isAuthenticated()) {
+    res.send('Hey, ' + req.user + ', hello from the server!');
+  } else {
+    res.redirect('/login');
+  }
+});
+app.get('/login', function(req, res) {
+  console.log(req.user);
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+app.post('/login', passport.authenticate('local', {
+  failureRedirect: '/login',
+  successRedirect: '/success'
+}));
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.send('Logged out!');
+});
+
+app.listen(8000, function() {
+  console.log("Ready for some authentication action");
+})
